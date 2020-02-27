8dd77bd92503ee2efec545914e04e7ec7786ce87
sisiban0 <493343269@qq.com>
2020-02-27 18:41:12 +0800

    服务表数据更改功能开发

--- mock/index.js
+++ mock/index.js
@@ -2,8 +2,10 @@
 let directive=require('./routes/directive')
 let data=require('./routes/data')
 let router=require('./routes/router')
+let server=require('./routes/server')
 module.exports = function (app) {
   directive(app);
   data(app);
   router(app);
+  server(app);
 }

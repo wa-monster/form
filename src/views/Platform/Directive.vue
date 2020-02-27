cebcd9499ae9a49172b8b062e2b4f25734c28946
sisiban0 <493343269@qq.com>
2020-02-27 11:54:34 +0800

    删除公用方法提取

--- src/views/Platform/Directive.vue
+++ src/views/Platform/Directive.vue
@@ -183,32 +183,6 @@ export default {
         throw err;
       }
     },
-    async delete(data, item,fn) {
-      try {
-        let params;
-        if(Array.isArray(item)){
-          params=item.map((item)=>{
-            return item.id;
-          })
-        }else{
-          params=[];
-          params.push(item.id);
-        }
-        let res = await fn(params);
-        data.forEach((i, index) => {
-          if (item.id === i.id) {
-            data.splice(index, 1);
-          }
-        });
-        if (data.length === 0) {
-          if (this.page.currentPage > 1) {
-            this.page.currentPage--;
-          }
-        }
-      } catch (err) {
-        throw err;
-      }
-    },
     closeDiglog(state, type) {
       if (type == "add") {
         this.addDialogVisible = state;
